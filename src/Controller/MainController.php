d7af1a33deaa9b575bcbb11d1af43b4f8fbf0c22
Timothee Robert <timothee.robert@gmail.com>
2025-07-17 19:14:12 +0200

    Intro Twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function pageaccueil()
     {
+        $nombreProduits = 23 ;
         // This method will handle the logic for the accueil page
         // For now, we can just return a simple message or view
-        return new Response('<h1>Welcome to the Accueil Page</h1><p>This is the main page of our application.</p>');
+        // return new Response('<h1>Welcome to the Accueil Page</h1><p>This is the main page of our application.</p>');
+        return $this->render('main/accueil.html.twig',['nombreProduits' => $nombreProduits ]);
     }
 }
